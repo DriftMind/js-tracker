cacf77f40534c63ee6cd0d72e31a3e0868d1c2f7
pilagod <hotony801018@gmail.com>
2016-10-05 20:11:47 +0800

    travis integrated with karma

--- karma.conf.js
+++ karma.conf.js
@@ -66,6 +66,15 @@ module.exports = function (config) {
     browsers: ['Chrome'],
 
 
+    // http://blog.500tech.com/setting-up-travis-ci-to-run-tests-on-latest-google-chrome-version/
+    customLaunchers: {
+      Chrome_travis_ci: {
+        base: 'Chrome',
+        flags: ['--no-sandbox']
+      }
+    }
+
+
     // Continuous Integration mode
     // if true, Karma captures browsers, runs the tests and exits
     singleRun: false,
@@ -74,4 +83,8 @@ module.exports = function (config) {
     // how many browser should be started simultaneous
     concurrency: Infinity
   })
+
+  if (process.env.TRAVIS) {
+    config.browsers = ['Chrome_travis_ci'];
+  }
 }
